754356def6d897f30b270072943b8ef0679e343f
Custom Software <kcluff50@gmail.com>
2023-12-11 08:24:08 -0700

    Initial Commit

---
+++ AltACS/AltACSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltACSApp.swift
+//  AltACS
+//
+//  Created by Kenneth Cluff on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AltACSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
